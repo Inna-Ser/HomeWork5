5b03bbd91328cb3b6eecd7cf771ef6c0320fac8b
Inna <serebryakovainna@mail.ru>
2022-10-31 06:37:56 +0200

    Внесли измменения 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 1;
-        boolean IOS = clientOS == 0;
-        if (IOS) {System.out.println("Установите версию приложения для iOS по ссылке.");
+        // ссылкка на приложение
+        int clientOS = 0;
+        int clientDeviceYear = 2017;
+        if(clientOS == 0) {
+            if(clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            }
+            else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            }
+            }
+        else if (clientOS == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            }
+            }
         }
-        else {
-            System.out.println("Установите версию приложения для Android по ссылке.");
-        }
-        }
-}
+    }
