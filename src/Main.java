0bede6cb2509a47beeeb3f8159ccc0cb3b02030f
Inna <serebryakovainna@mail.ru>
2022-11-01 19:38:19 +0200

    Внесли измменения 3

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,32 @@ public class Main {
         int clientDeviceYear = 2010;
         boolean a = clientDeviceYear >= 2015;
         if (a) {
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке.");
-                break;
-        }
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите версию приложения для iOS по ссылке.");
+                    break;
+                case 1:
+                    System.out.println("Установите версию приложения для Android по ссылке.");
+                    break;
+            }
         }
         if (!a) {
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите облегченную версию приложения для IOS по ссылке.");
-                break;
-            case 1:
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-                break;
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите облегченную версию приложения для IOS по ссылке.");
+                    break;
+                case 1:
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+                    break;
+            }
         }
+        // Высокосный год
+        int year = 2020;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + " год является високосным.");
+            }
+        else {
+            System.out.println(year + " год не является високосным.");
         }
     }
 }
