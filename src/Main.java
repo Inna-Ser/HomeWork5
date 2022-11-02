f30ca8e9dc6e9b6409580f9e3c54358426bc2376
Inna <serebryakovainna@mail.ru>
2022-11-02 05:13:39 +0200

    Внесли измменения 5

--- src/Main.java
+++ src/Main.java
@@ -46,5 +46,30 @@ public class Main {
         else {
             System.out.println("Доставки нет.");
         }
+        // Времена года
+        int monthNumber = 15;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Время года - зима.");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Время года - весна.");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Время года - лето.");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Время года - осень.");
+                break;
+            default: System.out.println("Такого месяца не существует.");
         }
     }
+}
